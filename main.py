38b933f596698f183f695ffd483efe724fed89e8
MarvinChung <marvin852316497@gmail.com>
2019-12-19 17:24:23 +0800

    sfm fixed

--- main.py
+++ main.py
@@ -307,8 +307,13 @@ def projectPoint(point, par_r, par_t, par_K):
     
 
                         
-def StructureFromMotion(imgs_combination, global_set, args):  
+def StructureFromMotion(imgs, global_set, args, MIN_REPROJECTION_ERROR=0.5):  
     
+    if args.isSeq == 1:
+        imgs_combination = getSequence(imgs)
+    else:
+        imgs_combination = getCombination(imgs)
+
     #3 dictionary
     par_K, par_r, par_t = read_pars(args)
     
@@ -351,7 +356,7 @@ def StructureFromMotion(imgs_combination, global_set, args):
             #uncomment will make it iterative bundle adjusmtnet and update global_set world points 
             #DrawPointClouds(global_set, ct+2, scale, par_K, par_r, par_t, debug = args.debug, show = False)
     
-    #DrawPointClouds(global_set, len(imgs), scale, par_K, par_r, par_t, debug = args.debug, show = True)
+    DrawPointClouds(global_set, len(imgs), args.scale, par_K, par_r, par_t, debug = args.debug, show = True)
                 
 
 import heapq
@@ -392,36 +397,56 @@ def DensePointsWithMVS(imgs, args):
     debug = args.debug
     wid = 5
     harris_features = []
+    harris_tables = []
     cutoff = 2
+    # expand_base = wid+1
+
 
     for img in imgs[:cutoff]:
         #expand space of the image for wid
-        expand_img = np.zeros((img.shape[0]+wid+1, img.shape[1]+wid+1, img.shape[2]), dtype='uint8')
-        for i in range(wid+1, img.shape[0]):
-            for j in range(wid+1, img.shape[1]):
-                expand_img[i,j,:] = img[i-wid-1, j-wid-1, :]
-        img = expand_img
-        harris_features.append(getHarrisPoints(img, debug = debug))
-    
+        # expand_img = np.zeros((img.shape[0] + 2 * expand_base, img.shape[1]+ 2 * expand_base, img.shape[2]), dtype='uint8')
+        # for i in range(expand_base, img.shape[0]):
+        #     for j in range(expand_base, img.shape[1]):
+        #         expand_img[i,j,:] = img[i-expand_base, j-expand_base, :]
+        # print(img.shape)
+        # img = expand_img
+        # print(img.shape)
+        harris_pts = getHarrisPoints(img, debug = debug)
+        harris_features.append(harris_pts)
+
+        #for P1 and P2 and opencv : The image origin is top-left, with x increasing horizontally, y vertically
+        #Therefore make image as size as img.shape[1], img.shape[0]
+        harris_table = np.full((img.shape[1],img.shape[0]), False)
+        for pt in harris_pts:
+            harris_table[pt[0], pt[1]] = True
+        harris_tables.append(harris_table)
     
     threshold = 0.5
     scale = args.scale
 
-    refined_src_features = []
+    refined_src_features = []   
     refined_dst_features = []
     
-    for idx_A,img in enumerate(imgs[:cutoff]):
+    for idx_A, img in enumerate(imgs[:cutoff]):
         filtered_coords1 = harris_features[idx_A]
-        for idx_B,cmp_img in enumerate(imgs[:cutoff]):
+        for idx_B, cmp_img in enumerate(imgs[:cutoff]):
             print(idx_A, idx_B)
-            if(idx_A != idx_B):        
+            if(idx_A != idx_B):
+
                 filtered_coords2 = harris_features[idx_B]
-                               
+
+                #for P1 and P2 and opencv : The image origin is top-left, with x increasing horizontally, y vertically
+                #Therefore make image as size as img.shape[1], img.shape[0]
+                src_is_seen = np.full((img.shape[1],img.shape[0]), False)
+                dst_is_seen = np.full((img.shape[1],img.shape[0]), False)
+                 
+                #use in method1 and method2               
                 d1 = getDescFeatures(img, filtered_coords1, wid) 
                 d2 = getDescFeatures(cmp_img, filtered_coords2, wid) 
                 
                 #method1
                 #the output matches have a threshold for NCC score which ensure photo consistency
+                
                 matches = MatchTwoSided(d1, d2, threshold = threshold)
                 
                 src_pts, dst_pts, ncc_scores = getMatches(img, cmp_img, filtered_coords1, filtered_coords2, matches, show_below=debug)
@@ -429,15 +454,13 @@ def DensePointsWithMVS(imgs, args):
                 F, mask = cv2.findFundamentalMat(src_pts, dst_pts, cv2.FM_RANSAC)
                 
                 heap = MyMatchHeap()
-                src_is_seen = {}
-                dst_is_seen = {}
                 if F is not None:
                     src_inliers = src_pts[mask.ravel()==1]       
                     dst_inliers = dst_pts[mask.ravel()==1]
                     ncc_inliers = ncc_scores[mask.ravel()==1]
                     for s,d,c in zip(src_inliers, dst_inliers, ncc_inliers):
-                        src_is_seen[(s[0],s[1])] = True
-                        dst_is_seen[(d[0],d[1])] = True
+                        src_is_seen[s[0],s[1]] = True
+                        dst_is_seen[d[0],d[1]] = True
                         # print(s,d,c)
                         # s array([447,  22], dtype=int32)
                         # d array([443,  16], dtype=int32)
@@ -458,26 +481,65 @@ def DensePointsWithMVS(imgs, args):
                     print("F is none")
                     raise RuntimeError
                 
-                """       
-                #method2
-                heap = MyMatchHeap()
-                src_is_seen = {}
-                dst_is_seen = {}
-                match_scores = Match(d1, d2, threshold=0.5)
-                for i,m in enumerate(match_scores): 
-                    if m>0:
-                        s = np.array([filtered_coords1[i][1],filtered_coords1[i][0]])
-                        d = np.array([filtered_coords2[m][1],filtered_coords2[m][0]])
-                        src_is_seen[(s[0],s[1])] = True
-                        dst_is_seen[(d[0],d[1])] = True
-                        #try:
-                        print(s,d,m)
-                        heap.push(MyMatch(s,d,m))
-                        #except:
-                        #    pdb.set_trace()
+                #end method1    
+
                 """
-                    
+                #method2
+                F = getORBFeatures(img, cmp_img, debug = debug, return_F = True)
+
+                heap = MyMatchHeap()
+                lines = cv2.computeCorrespondEpilines(filtered_coords1, 1, F).reshape(-1,3)
+
+                if debug:
+                    cmp_img_with_lines, right = drawlines(cmp_img, img, lines, filtered_coords2, filtered_coords1)                  
+                    fig=plt.figure(figsize=(64, 64))
+                    fig.suptitle('draw Epilines on both images(first)')
+                    fig.add_subplot(1,2,1)
+                    plt.imshow(cmp_img_with_lines, aspect='auto')
+                    fig.add_subplot(1,2,2)
+                    plt.imshow(right, aspect='auto')
+                    plt.show()
+
+
+                for line, left_pt in zip(lines, filtered_coords1):
+                    #temp_heap find the best ncc value of the src point and another dst point on the corresponding epipolar line
+                    temp_heap = MyMatchHeap()
+                    for i in range(-2,3,1):
+                        a = line[0]
+                        b = line[1]
+                        c = line[2]
+                        for x in range(cmp_img.shape[1]):
+                            #i: up down 2 pixel
+                            y = int(round((-a*x-c)/b + i))
+
+                            if y < cmp_img.shape[0] and y >= 0 and dst_is_seen[x,y] == False :
+                                if  harris_tables[idx_B][x,y] == True:
+                                    left_des = getDescFeatures(img, [left_pt], wid=wid)[0]
+                                    right_des = getDescFeatures(cmp_img, [np.array([x,y])], wid=wid)[0]
+                                    if type(left_des) != type(None) and type(right_des) != type(None) and left_des.shape == right_des.shape:
+                                        ncc_value = ctNcc(left_des, right_des)
+                                        #print("temp heap value:",ncc_value)
+                                        temp_heap.push(MyMatch(left_pt, np.array([x,y]), ncc_value))
+                    if(temp_heap.size()!=0):
+                        match_obj = temp_heap.pop()
+                        # src_point and dst_point is match, therefore don't seen them again
+                        if(src_is_seen[match_obj.src_point[0],match_obj.src_point[1]] == True):
+                            print("wtf src")
+                            input("")
+                        if(dst_is_seen[match_obj.dst_point[0],match_obj.dst_point[1]] == True):
+                            print("wtf dst")
+                            input("")
+                        src_is_seen[match_obj.src_point[0],match_obj.src_point[1]] = True
+                        dst_is_seen[match_obj.dst_point[0],match_obj.dst_point[1]] = True
+                        heap.push(match_obj)
+                #end method 3
+                """    
                 while(heap.size() != 0):
+                    print(np.count_nonzero(src_is_seen))
+                    print(np.count_nonzero(dst_is_seen))
+                    print(src_is_seen.shape)
+                    print(dst_is_seen.shape)
+                    print("heap size:", heap.size())
                     match_obj = heap.pop()
                     src_point = match_obj.src_point
                     dst_point = match_obj.dst_point
@@ -486,34 +548,60 @@ def DensePointsWithMVS(imgs, args):
                     refined_dst_features.append(dst_point.astype('float32'))
                     
                     src_min_x = max(0,src_point[0] - wid)
-                    src_max_x = min(img.shape[0]-1,src_point[0] + wid)
+                    src_max_x = min(img.shape[1]-1,src_point[0] + wid)
                     src_min_y = max(0,src_point[1] - wid)
-                    src_max_y = min(img.shape[1]-1,src_point[1] + wid)
+                    src_max_y = min(img.shape[0]-1,src_point[1] + wid)
                     
                     dst_min_x = max(0,dst_point[0] - wid)
-                    dst_max_x = min(cmp_img.shape[0]-1,dst_point[0] + wid)
+                    dst_max_x = min(cmp_img.shape[1]-1,dst_point[0] + wid)
                     dst_min_y = max(0,dst_point[1] - wid)
-                    dst_max_y = min(cmp_img.shape[1]-1,dst_point[1] + wid)
+                    dst_max_y = min(cmp_img.shape[0]-1,dst_point[1] + wid)
+                    """
                     #Add new potential matches in their immediate spatial neighborhood into heap
                     for i in range(src_min_x, src_max_x):
                         for j in range(src_min_y, src_max_y):
-                            if(src_is_seen[(i,j)]== False):
+                            if(src_is_seen[i,j] == False):
+                                #temp_heap find the best ncc value of the src point and dst point of this patch 
+                                temp_heap = MyMatchHeap()
                                 for k in range(dst_min_x, dst_max_x):
                                     for l in range(dst_min_y, dst_max_y):
-                                        if(dst_is_seen[(k,l)]==False):
-                                            ncc_value = ctNcc(getDescFeatures(img, [np.array(i,j)], wid=wid)[0],getDescFeatures(cmp_img, [np.array(k,l)], wid=wid))
-                                            print("ncc_value",ncc_value)
-                                            if(1 - ncc_value > 0.2):
-                                                print("allow")
-                                                
-                                                heap.push(MyMatch([np.array(i,j)],[np.array(k,l)], ncc_value))
-                                                src_is_seen[(i,j)] = True
-                                                dst_is_seen[(k,l)] = True
-                                                
+                                        if(dst_is_seen[k,l] == False):
+                                            left_des = getDescFeatures(cv2.cvtColor(img,cv2.COLOR_BGR2GRAY), [np.array([i,j])], wid=wid)[0]
+                                            right_des = getDescFeatures(cv2.cvtColor(cmp_img,cv2.COLOR_BGR2GRAY), [np.array([k,l])], wid=wid)[0]
+                                            if type(left_des) != type(None) and type(right_des) != type(None) and left_des.shape == right_des.shape:
+                                                ncc_value = ctNcc(left_des, right_des)
+                                                #print("ncc_value",ncc_value)
+                                                if (ncc_value == math.nan):
+                                                    pdb.set_trace()
+                                                if(1 - ncc_value > 0.5):
+                                                    temp_heap.push(MyMatch(np.array([i,j]),np.array([k,l]), ncc_value))
+                                if temp_heap.size() != 0:
+                                    match_obj = temp_heap.pop()
+
+                                    if(src_is_seen[match_obj.src_point[0],match_obj.src_point[1]] == True):
+                                        print("wtf src")
+                                        input("")
+                                    if(dst_is_seen[match_obj.dst_point[0],match_obj.dst_point[1]] == True):
+                                        print("wtf dst")
+                                        input("")
+
+                                    # src_point and dst_point is match, therefore don't seen them again
+                                    src_is_seen[match_obj.src_point[0],match_obj.src_point[1]] = True
+                                    dst_is_seen[match_obj.dst_point[0],match_obj.dst_point[1]] = True
+                                    heap.push(match_obj)
+                    #Only construct a point in a patch
+                    for i in range(src_min_x, src_max_x):
+                        for j in range(src_min_y, src_max_y):
+                            for k in range(dst_min_x, dst_max_x):
+                                    for l in range(dst_min_y, dst_max_y):
+                                        src_is_seen[i,j] = True
+                                        dst_is_seen[k,l] = True
+                    """                            
                 P1 = getProjectionMatrix(par_K[idx_A], par_r[idx_A], par_t[idx_A])
                 P2 = getProjectionMatrix(par_K[idx_B], par_r[idx_B], par_t[idx_B])
                 
                 #abort trap in cv2.triangulatePoints if query_inliers or train_inliers are type int
+                
                 points = traingulatePoints(P1, P2, np.array(refined_src_features), np.array(refined_dst_features))
                 #points = traingulatePoints(P1, P2, np.concatenate((np.array(refined_src_features),np.array([1 for i in range(len(refined_src_features))]).reshape(-1,1)),axis=1), np.concatenate((np.array(refined_dst_features),np.array([1 for i in range(len(refined_dst_features))]).reshape(-1,1)),axis=1)) 
                 ax = plt.axes(projection='3d')
@@ -565,9 +653,9 @@ def DrawPointClouds(global_set, n_cameras, scale, par_K, par_r, par_t, debug = F
     
     #add camera position
     if show == True:
-#         for i in range(len(par_t)):
-#             draw_points.append(-(par_r[i].transpose()@par_t[i]).ravel())
-#             draw_color.append('pink')
+        # for i in range(len(par_t)):
+        #     draw_points.append(-(par_r[i].transpose()@par_t[i]).ravel())
+        #     draw_color.append('pink')
 
         ax = plt.axes(projection='3d')
         ax.set_title('without bundle adjustment')
@@ -621,11 +709,11 @@ def DrawPointClouds(global_set, n_cameras, scale, par_K, par_r, par_t, debug = F
         draw_color = ['blue' for i in range(len(draw_points))]
         if show == True:
             #add camera position
-    #         for i in range(n_cameras):
-    #             refine_par_t = res.x[i*11+3:i*11+6]
-    #             refine_par_r, jacobian = cv2.Rodrigues(res.x[i*11:i*11+3])
-    #             draw_points.append(-(refine_par_r.transpose() @ refine_par_t.reshape(3,-1)).ravel())
-    #             draw_color.append('pink')
+            # for i in range(n_cameras):
+            #     refine_par_t = res.x[i*11+3:i*11+6]
+            #     refine_par_r, jacobian = cv2.Rodrigues(res.x[i*11:i*11+3])
+            #     draw_points.append(-(refine_par_r.transpose() @ refine_par_t.reshape(3,-1)).ravel())
+            #     draw_color.append('pink')
 
             ax = plt.axes(projection='3d')
             ax.set_title('with bundle adjustment')
@@ -668,14 +756,10 @@ def main(args, threshold = 0.01, MIN_REPROJECTION_ERROR = 0.3):
     scale = args.scale
     imgs = read_imgs(args)
 
-    if args.isSeq == 1:
-        imgs_combination = getSequence(imgs)
-    else:
-        imgs_combination = getCombination(imgs)
         
     global_set = GlobalSet(threshold = threshold)    
         
-    #StructureFromMotion(imgs_combination, global_set, args)
+    #StructureFromMotion(imgs, global_set, args, MIN_REPROJECTION_ERROR)
     DensePointsWithMVS(imgs, args) 
 
 if __name__== "__main__":
